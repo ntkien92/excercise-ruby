d8a6a2327a193e5fb480a93d777ca5978b47bc66
NTKIEN92 <ntkien92@gmail.com>
2015-09-13 18:12:06 +0700

    [MODIFY] add comment to the project
    add comment to explain some function

--- Tax/Main.rb
+++ Tax/Main.rb
@@ -2,13 +2,17 @@ require './ManageTax'
 require './ReadFileManager'
 
 def main
+	#To read file from input_tax.txt
 	read_file = ReadFileManager.new("input_tax.txt")
+	#create array product from readfile
 	array_product = read_file.read_input_file
+	#show product from array product
 	array_product.each {|product| puts product.show_detail}
+	#create manage_tax to calculation sum tax, sum price
 	manage_tax = ManageTax.new(array_product)
 	puts "Tong thue: " << manage_tax.sum_tax.to_s
 	puts "Tong gia tri: " << (manage_tax.sum_tax + manage_tax.sum_price).to_s
-
+	#write output to the file
 	write_file = ReadFileManager.new("output_tax.txt")
 	write_file.write_output_file manage_tax
 end
--- Tax/ReadFileManager.rb
+++ Tax/ReadFileManager.rb
@@ -3,25 +3,33 @@ class ReadFileManager
 	def initialize file_name
 		@file_name = file_name
 	end
+	#read input file
 	def read_input_file
 		file = File.open(@file_name,"r")
 		array_product = Array.new
 		if file
 			while (line = file.gets)
+				#read a line product, line is a string, so that, we can split to 
+				#three parameters, first is number, second is product, last is price
 				array_split = line.split(',')
 				product = Product.new(array_split[0].to_i, array_split[1], array_split[2].to_i)			
+				#add to array
 				array_product << product
 			end
 		else
 			puts "Cannot open file"
 		end
 		file.close
+		#return array list product
 		array_product
 	end
+	#to show output into filename
+
 	def write_output_file manage_tax
 		file = File.open(@file_name,"w")
 			if file
 				file.puts "Number \t Product \t Price"
+				#show product_details
 				manage_tax.array_product.each {|product| file.puts product.show_detail}
 				file.print "Tong so thue: " 
 				file.puts manage_tax.sum_tax
